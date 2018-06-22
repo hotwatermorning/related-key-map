8720d9b158eea33937343f08110b75f7db7cee52
yuasa <hotwatermorning@gmail.com>
2018-06-22 22:37:37 +0900

    use postcss-preset-env instead of postcss-cssnext

--- postcss.config.js
+++ postcss.config.js
@@ -6,6 +6,6 @@ module.exports = {
         require("postcss-nested"),
         require("postcss-for"),
         require("postcss-extend"),
-        require("postcss-cssnext"),
+        require("postcss-preset-env"),
     ]
 };
