ce4f61249563cee2ba6f1f4692b93efbf9da46d2
yuasa <hotwatermorning@gmail.com>
2018-06-27 15:50:22 +0900

    enable response compression

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require("compression");
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+app.use(compression());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
